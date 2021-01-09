67a0eacb712a0522f61fd25d87bc35073a6bf95c
robzgf <rgrielfilho@gmail.com>
2021-01-08 21:36:01 -0300

    Aulas de Variáveis no PHP

---
+++ variaveis/exemplo-01.php
@@ -0,0 +1,10 @@
+<?php
+//Variáveis são espaços na memória e que podem ser acessadas ou alteradas no código
+
+$nomeCompleto = "Roberto Griel";
+$primeiroNome = "Roberto";
+$segundoNome = "Griel";
+
+echo $nomeCompleto;
+echo "<hr/>";
+echo $primeiroNome . " " . $segundoNome;
